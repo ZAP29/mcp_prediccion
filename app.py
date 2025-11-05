dfc03c8d0c3d1d58a05261ec5f337e7c7ace0c57
Diego Zapata <dialzaur29@gmail.com>
2025-11-04 22:50:35 -0500

    se agrega un puerto dinamico

--- app.py
+++ app.py
@@ -66,3 +66,9 @@ def predecir_precio(data: InputData):
 
     except Exception as e:
         return {"error": str(e)}
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
